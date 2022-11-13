ea978af291abd3a12347d5da105f35be12cad7ec
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2022-11-13 12:15:04 -0500

    adding a bit of functionality
    I used the addEventListener() method to detect when the user clicks on these buttons.

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,11 @@ let initialCards = [{
         link: src = "./images/lagodeibraes.jpg"
     }
 ];
+
+let modalBoxOpen = document.querySelector('.edit_button');
+let modalDisplay = document.querySelector(".modal_box")
+
+function Opener() {
+    modalDisplay.setAttribute("style", "display:flex");
+}
+modalBoxOpen.addEventListener("click", Opener);
