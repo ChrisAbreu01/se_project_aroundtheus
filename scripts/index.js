122aae08710d1a24f135f9b793a0d4233a8a3f7d
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2022-11-15 18:11:23 -0500

    Project 4. Final Stage v1
    1) interactivity was integrated in modal box
    2) function to edit name and description was added
    3) cards are been created from JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,93 @@
 let initialCards = [{
         name: "Yosemite valley",
-        link: src = "./images/yosemity.jpg"
+        link: "./images/yosemity.jpg"
     },
     {
         name: "Lake Louise",
-        link: src = "./images/lakelois.jpg"
+        link: "./images/lakelois.jpg"
     },
     {
         name: "Bald Mountains",
-        link: src = "./images/baldmountains.jpg"
+        link: "./images/baldmountains.jpg"
     },
     {
         name: "Latemar",
-        link: src = "./images/latemar.jpg"
+        link: "./images/latemar.jpg"
     },
     {
         name: "Vanoise National Park",
-        link: src = "./images/vanoise.jpg"
+        link: "./images/vanoise.jpg"
     },
     {
         name: "Lago di Braies",
-        link: src = "./images/lagodeibraes.jpg"
+        link: "./images/lagodeibraes.jpg"
     }
 ];
 
-let modalBoxOpen = document.querySelector('.edit_button');
-let modalDisplay = document.querySelector(".modal_box")
+let modalBoxOpen = document.querySelector('.profile__edit-space');
+let modalBoxClose = document.querySelector('.modal_close-button');
+let modalDisplay = document.querySelector(".modal_box");
+let modalLabelDefault1 = document.querySelector(".modallabel1");
+let modalLabelDefault2 = document.querySelector(".modallabel2");
+let profileTitle = document.querySelector(".profile__name").textContent;
+let profileSubtitle = document.querySelector(".profile__subtitle").textContent;
+let cardElementSelector = document.querySelector(".elements");
+console.log(cardElementSelector);
+const cardTemplate = document.querySelector("#elements-template").content.querySelector('.elements__element');
+console.log(cardTemplate);
 
 function Opener() {
     modalDisplay.setAttribute("style", "display:flex");
+    modalLabelDefault1.textContent = profileTitle;
+    modalLabelDefault2.textContent = profileSubtitle;
+}
+
+function closer() {
+    modalDisplay.setAttribute("style", "display:none");
 }
 modalBoxOpen.addEventListener("click", Opener);
+modalBoxClose.addEventListener("click", closer);
+
+
+const profileFormElement = document.querySelector(".modal_form");
+const formElement = document.querySelector(".modal_sumit-button");
+
+
+const nameInput = document.querySelector(".modal_input-1");
+const jobInput = document.querySelector(".modal_input-2");
+
+
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__subtitle");
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = (document.querySelector('.modal_input-1').value);
+    profileJob.textContent = (document.querySelector('.modal_input-2').value);
+
+    closer();
+}
+formElement.addEventListener('click', handleProfileFormSubmit);
+
+function getCardElement(data) {
+    let cardElement = cardTemplate.cloneNode(true);
+    console.log(cardElement);
+    let cardImage = cardElement.querySelector(".elements__img");
+    let cardTitle = cardElement.querySelector(".elements__title");
+    console.log(cardImage);
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    console.log(cardTitle);
+    console.log(cardElement);
+    return cardElement;
+
+
+}
+for (let i = 0; i < initialCards.length; i++) {
+    const item = initialCards[i];
+    console.log(item);
+    const cardElement = getCardElement(item);
+    cardElementSelector.append(cardElement);
+}
