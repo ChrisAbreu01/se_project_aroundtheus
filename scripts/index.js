0263212ac765528adb69f5c38b5bc1cdfbcccad9
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2022-12-16 00:21:57 -0500

    Final Project #5 v7
    optimizing functionality in JavaScript
    deleting unnecessary functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
-let initialCards = [{
-        name: "Yosemite valley",
-        link: "./images/yosemity.jpg"
-    },
-    {
-        name: "Lake Louise",
-        link: "./images/lakelois.jpg"
-    },
-    {
-        name: "Bald Mountains",
-        link: "./images/baldmountains.jpg"
-    },
-    {
-        name: "Latemar",
-        link: "./images/latemar.jpg"
-    },
-    {
-        name: "Vanoise National Park",
-        link: "./images/vanoise.jpg"
-    },
-    {
-        name: "Lago di Braies",
-        link: "./images/lagodeibraes.jpg"
-    }
+const initialCards = [{
+    name: "Yosemite valley",
+    link: "./images/yosemity.jpg"
+},
+{
+    name: "Lake Louise",
+    link: "./images/lakelois.jpg"
+},
+{
+    name: "Bald Mountains",
+    link: "./images/baldmountains.jpg"
+},
+{
+    name: "Latemar",
+    link: "./images/latemar.jpg"
+},
+{
+    name: "Vanoise National Park",
+    link: "./images/vanoise.jpg"
+},
+{
+    name: "Lago di Braies",
+    link: "./images/lagodeibraes.jpg"
+}
 ];
 
 const profileModalBoxOpen = document.querySelector('.profile__edit-space');
@@ -43,53 +43,50 @@ const cardUrlInput = document.querySelector(".card__modal-input-2");
 const imageModalClose = document.querySelector(".popup__close");
 const imageView = document.querySelector(".popup");
 
-function openProfileModal() {
-    profileModalDisplay.classList.add("modal_box_opened");
+function openModal(modal) {
+    modal.classList.add("modal_box_opened");
+}
+function closeModal(modal) {
+    modal.classList.remove("modal_box_opened");
+}
+profileModalBoxOpen.addEventListener("click", () => {
+    openModal(profileModalDisplay)
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-
-}
-
-function closeProfileModal() {
-    profileModalDisplay.classList.remove("modal_box_opened");
-}
-function openCardModal() {
-    cardModalDisplay.classList.add("card__modal-box-opened");
-
-}
-
-function closeCardModal() {
-    cardModalDisplay.classList.remove("card__modal-box-opened");
-}
-profileModalBoxOpen.addEventListener("click", openProfileModal);
-profileModalBoxClose.addEventListener("click", closeProfileModal);
-cardModalBoxOpen.addEventListener("click", openCardModal);
-cardModalBoxClose.addEventListener("click", closeCardModal);
-
-
+});
+profileModalBoxClose.addEventListener("click", () => { closeModal(profileModalDisplay) });
+cardModalBoxOpen.addEventListener("click", () => { openModal(cardModalDisplay) });
+cardModalBoxClose.addEventListener("click", () => { closeModal(cardModalDisplay) });
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
 
-    profileName.textContent = (nameInput.value);
-    profileJob.textContent = (jobInput.value);
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 
-    closeProfileModal()
+    closeModal(profileModalDisplay);
 }
 
 
-profileFormElement.addEventListener('submit',handleProfileFormSubmit );
+
 function getCardElement(data) {
     const cardElement = cardTemplate.cloneNode(true);
     const cardImage = cardElement.querySelector(".elements__img");
     const cardTitle = cardElement.querySelector(".elements__title");
+    const cardLikeButtom = cardElement.querySelector(".elements__like-buttom");
+    const deleteCard = cardElement.querySelector(".element__delete-buttom");
     cardImage.src = data.link;
     cardImage.alt = data.name;
     cardTitle.textContent = data.name;
     cardImage.addEventListener('click', () => {
         handlePreviewPicture(data);
     })
-
+    cardLikeButtom.addEventListener("click", () => cardLikeButtom.classList.toggle("elements__like-buttom-black"));
+    deleteCard.addEventListener("click", function () {
+        const listItem = deleteCard.closest(".elements__element");
+        listItem.remove();
+    });
     return cardElement;
 }
 const imageElement = document.querySelector(".popup__image");
@@ -99,53 +96,29 @@ function handlePreviewPicture(data) {
     imageElement.src = data.link;
     imageElement.alt = data.name;
     imageCaption.textContent = data.name;
-    openModal();
+    openModal(imageView);
 }
-function openModal () {
-    imageView.classList.add("popup_is-opened");
-    
-}
-function closeModal (){
-    imageView.classList.remove("popup_is-opened");
-}
-imageModalClose.addEventListener("click", closeModal);
-function generateCards (){
+imageModalClose.addEventListener("click", () => { closeModal(imageView) });
+function generateCards() {
     initialCards.forEach(cardData => {
         const cardElement = getCardElement(cardData);
         cardsContainer.prepend(cardElement);
-        
+
     })
 }
-generateCards ();
+generateCards();
 
-function addEventToCards (data){
-    const cardLikeButtom = document.querySelectorAll(".elements__like-buttom");    
-    cardLikeButtom.forEach(data =>{
-        data.addEventListener("click",() => data.classList.toggle("elements__like-buttom-black"));
-    })    
-}
-addEventToCards ();
 function handleCardFormSubmit(evt) {
     evt.preventDefault();
 
-    initialCards.unshift({name:cardTitleInput.value , link:cardUrlInput.value});
-    const cardElement = getCardElement(initialCards[0]);
-    cardsContainer.prepend(cardElement);
-    const newCardLikeButtom = document.querySelector(".elements__like-buttom");
-    newCardLikeButtom.addEventListener("click",() => newCardLikeButtom.classList.toggle("elements__like-buttom-black"));
+    const cardElement = getCardElement({
+        name: cardTitleInput.value,
+        link: cardUrlInput.value
+     })
+     cardsContainer.prepend(cardElement); 
     deleteCardHandler();
-    closeCardModal();
+    closeModal(cardModalDisplay);
 }
-function deleteCardHandler(){
-const deleteCard = document.querySelectorAll(".element__delete-buttom");
-deleteCard.forEach(data => {
-    data.addEventListener("click", function() {
-        const listItem = data.closest(".elements__element");
-        listItem.remove();
-    });
-})
-}
-deleteCardHandler();
 
-cardFormElement.addEventListener('submit',handleCardFormSubmit );
+cardFormElement.addEventListener('submit', handleCardFormSubmit);
 
